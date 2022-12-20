f24c962ba64bb4405f00af938b3d0b9fed5dc25c
Nickson Santos Endlich <nickson12@gmail.com>
2022-12-20 17:54:57 +0000

    29. Enriching the created domain model

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -2,13 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            // Name = name ?? throw new ArgumentException("name is invalid");
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            Name = name;
+        }
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Too short. Minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
