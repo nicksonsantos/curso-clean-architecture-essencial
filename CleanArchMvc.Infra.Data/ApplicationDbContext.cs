aecf138f40e1a939b11326fa8b55c3a53d6c3f7f
Nickson Santos Endlich <nickson12@gmail.com>
2022-12-23 13:18:18 +0000

    36. Infra.Data - Creating context

---
+++ CleanArchMvc.Infra.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
